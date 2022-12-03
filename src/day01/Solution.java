f67b6197ae4512b8bc27dbe94ef0ab4a9a11552f
Johan Lundgren <lundgren.johan99@gmail.com>
2022-12-03 22:28:06 +0100

    template update

--- src/day01/Solution.java
+++ src/day01/Solution.java
@@ -1,4 +1,17 @@
 package day01;
 
+import static utils.ReadFile.readFileAsIntegerArray;
+
 public class Solution {
+    public static void main(String[] args) throws Exception {
+        Integer[] input = readFileAsIntegerArray("src\\day01\\input.txt");
+        part1(input);
+        part2(input);
+    }
+
+    private static void part1(Integer[] input) {
+    }
+
+    private static void part2(Integer[] input) {
+    }
 }
