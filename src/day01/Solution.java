2c6cc05e7df423d863ffb60a1424d493407aa821
Johan Lundgren <lundgren.johan99@gmail.com>
2022-12-03 22:31:52 +0100

    Updated utils utilization

--- src/day01/Solution.java
+++ src/day01/Solution.java
@@ -1,17 +1,20 @@
 package day01;
 
+import static utils.ReadFile.readFileAsArray;
 import static utils.ReadFile.readFileAsIntegerArray;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        Integer[] input = readFileAsIntegerArray("src\\day01\\input.txt");
+        String[] input = readFileAsArray("src\\day01\\input.txt");
         part1(input);
         part2(input);
     }
 
-    private static void part1(Integer[] input) {
+    private static void part1(String[] input) {
+        // Din lösning här för del 1
     }
 
-    private static void part2(Integer[] input) {
+    private static void part2(String[] input) {
+        // Din lösning här för del 2
     }
 }
